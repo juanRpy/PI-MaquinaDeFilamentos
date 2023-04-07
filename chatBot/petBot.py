8c8a6ed6baa080b141dbe2c06c3d00ffd1676457
Juan José Rivera <juanjoserivera10@outlook.com>
2023-04-07 13:32:41 -0500

    Telegram_Token

--- chatBot/petBot.py
+++ chatBot/petBot.py
@@ -1,6 +1,6 @@
 import telebot
-
-bot = telebot.TeleBot("6231150170:AAHII5endnvpDu0sH70dX4cHsUrY4iq_0E8")
+from confing import *
+bot = telebot.TeleBot()
 @bot.message_handler(commands = ["help", "start"])
 
 def enviar (message):
